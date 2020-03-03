594ff43eb8e03c4e49414c496172de8aafce86b3
tranphuquy19 <tranphuquy19@gmail.com>
2020-03-04 02:44:39 +0700

    GET records & GET a record

---
+++ index.js
@@ -0,0 +1,62 @@
+/*
+ * Created by @tranphuquy19 on 03/03/2020
+ * @author: tranphuquy19@gmail.com
+ */
+'use strict'
+const {getAllDnsRecords, getDnsRecordById} = require('./lib/dns');
+
+Object.prototype.isEmpty = function () {
+    for (let key in this) {
+        if (this.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
+// module.exports = function () {
+//     let opt;
+//     this.config = (auth) => {
+//         if (auth.isEmpty()) {
+//             new Error("Authentication information is empty!");
+//         } else {
+//             opt = auth;
+//         }
+//         return {
+//             getAllDnsRecords,
+//             getDnsRecordById
+//         }
+//     }
+// };
+module.exports = class CloudflareDdns {
+    constructor(opt) {
+        this.token = opt.token;
+        this.key = opt.key;
+        this.email = opt.email;
+        this.zoneId = opt.zoneId;
+    }
+
+    getAllDnsRecords () {
+        if (this.zoneId.isEmpty()) {
+            throw new Error('Zone ID is missing');
+        } else {
+            return getAllDnsRecords({...this});
+        }
+    };
+
+    getDnsRecordById(dnsId){
+        if(dnsId.isEmpty()){
+            throw new Error("DNS ID is missing");
+        }else {
+            return getDnsRecordById(dnsId, {...this});
+        }
+    }
+
+    checkAuth(){
+        if(this.key.isEmpty() || this.email.isEmpty() ){
+            throw new Error("")
+        }
+    }
+    toString(cb = JSON.stringify) {
+        console.log(cb({token: this.token, key: this.key, email: this.email}));
+    }
+}
